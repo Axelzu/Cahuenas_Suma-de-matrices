b9a2f979701c2aa4cbf17fef137e990fad218b08
Alexander Cahueñas <Alexander Cahueas>
2023-11-28 15:10:27 -0500

    2

--- main.c
+++ main.c
@@ -4,6 +4,14 @@
 
 int main() {
     
+    printf("Ingrese el número de filas de las matrices: ");
+    scanf("%d", &filas);
+    printf("Ingrese el número de columnas de las matrices: ");
+    scanf("%d", &columnas);
 
+    int matriz1[filas][columnas];
+    int matriz2[filas][columnas];
+    int resultado[filas][columnas];
+    
     return 0;
 }
